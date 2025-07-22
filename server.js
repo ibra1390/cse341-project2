10c23c5a868f13c7b52e3c1ec9c5eacdfd0b12ef
ibra1390 <ing.gonzalezvij@hotmail.com>
2025-07-22 17:01:32 -0600

    fixing errors

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ passport.deserializeUser((user, done) => {
 app.get('/', (req, res) => {res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out")});
 
 app.get('/github/callback', passport.authenticate('github', {
-    failureRedirect: '/api-docs', session: false}),
+    failureRedirect: '/api-docs' }),
     (req, res) => {
     req.session.user = req.user;
     res.redirect('/');
